0ed79f5ada86c979b7b2159d972ba53cc5bb70e8
hankelc <taichen.liu@imorpheus.io>
2021-07-01 11:22:06 +0800

    add android x86_64 building shell

---
+++ swig/build-android-x86_64.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# This script is meant to run here inside the swig folder
+# It's supposed to be a one step build for the java jar and android (.so enclosing) jars (armv7 and x86)
+# Output .jar files will be at:
+# ../build/libs/jlibtorrent-<version>.jar
+# ../build/libs/${LIBRARY_NAME}-android-x86_64-<version>.jar
+
+# remote android-x86 build with travis is available at https://s3.amazonaws.com/gubatron-${LIBRARY_NAME}/release/android/x86_64/lib${LIBRARY_NAME}.so
+
+source build-utils-android.shinc
+check_min_req_vars
+
+export os_arch=x86_64
+export os_build=android
+export android_api=22
+export SHARED_LIB=${LIBRARY_NAME}.so
+export SHARED_LIB_FINAL=${SHARED_LIB} # dummy for macosx
+
+export CXX=g++
+export NDK_VERSION=r22b
+
+prepare_android_toolchain
+abort_if_var_unset "ANDROID_TOOLCHAIN" ${ANDROID_TOOLCHAIN}
+export CC=$ANDROID_TOOLCHAIN/bin/x86_64-linux-android-clang
+
+export run_openssl_configure="./Configure linux-x86_64 ${OPENSSL_NO_OPTS} -fPIC -mstackrealign --prefix=${OPENSSL_ROOT}";
+export run_readelf="${ANDROID_TOOLCHAIN}/bin/x86_64-linux-android-readelf -d bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
+export run_bjam="${BOOST_ROOT}/b2 -j8 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=clang-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}"
+export run_strip="${ANDROID_TOOLCHAIN}/bin/x86_64-linux-android-strip --strip-unneeded -x -g bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
+export run_objcopy="${ANDROID_TOOLCHAIN}/bin/x86_64-linux-android-objcopy --only-keep-debug bin/release/${os_build}/${os_arch}/${SHARED_LIB} bin/release/${os_build}/{$os_arch}/${SHARED_LIB}.debug"
+export PATH=$ANDROID_TOOLCHAIN/x86_64-linux-android/bin:$PATH;
+sed -i 's/RANLIB = ranlib/RANLIB = "${ANDROID_TOOLCHAIN}\/bin\/x86_64-linux-android-ranlib"/g' ${BOOST_ROOT}/tools/build/src/tools/gcc.jam;
+
+create_folder_if_it_doesnt_exist ${SRC}
+prompt_msg "About to prepare BOOST ${BOOST_VERSION}"
+press_any_to_continue
+#prepare_boost
+#prepare_openssl
+#build_openssl
+#prepare_libtorrent
+build_libraries
+#cleanup_objects
