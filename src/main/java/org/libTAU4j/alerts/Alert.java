fe10ce9267c971fec6322a36f97ee13863599df8
hankelc <taichen.liu@imorpheus.io>
2021-09-29 20:12:35 +0800

    modify alerts for delete and adding

--- src/main/java/org/libTAU4j/alerts/Alert.java
+++ src/main/java/org/libTAU4j/alerts/Alert.java
@@ -15,24 +15,16 @@ public interface Alert<T extends alert> {
     alert_category_t PEER_NOTIFICATION = alert.peer_notification;
     alert_category_t PORT_MAPPING_NOTIFICATION = alert.port_mapping_notification;
     alert_category_t STORAGE_NOTIFICATION = alert.storage_notification;
-    alert_category_t TRACKER_NOTIFICATION = alert.tracker_notification;
     alert_category_t CONNECT_NOTIFICATION = alert.connect_notification;
     alert_category_t STATUS_NOTIFICATION = alert.status_notification;
-    alert_category_t IP_BLOCK_NOTIFICATION = alert.ip_block_notification;
     alert_category_t PERFORMANCE_WARNING = alert.performance_warning;
     alert_category_t DHT_NOTIFICATION = alert.dht_notification;
     alert_category_t SESSION_LOG_NOTIFICATION = alert.session_log_notification;
-    alert_category_t TORRENT_LOG_NOTIFICATION = alert.torrent_log_notification;
-    alert_category_t PEER_LOG_NOTIFICATION = alert.peer_log_notification;
     alert_category_t INCOMING_REQUEST_NOTIFICATION = alert.incoming_request_notification;
     alert_category_t DHT_LOG_NOTIFICATION = alert.dht_log_notification;
     alert_category_t DHT_OPERATION_NOTIFICATION = alert.dht_operation_notification;
     alert_category_t PORT_MAPPING_LOG_NOTIFICATION = alert.port_mapping_log_notification;
-    alert_category_t PICKER_LOG_NOTIFICATION = alert.picker_log_notification;
-    alert_category_t FILE_PROGRESS_NOTIFICATION = alert.file_progress_notification;
-    alert_category_t PIECE_PROGRESS_NOTIFICATION = alert.piece_progress_notification;
     alert_category_t UPLOAD_NOTIFICATION = alert.upload_notification;
-    alert_category_t BLOCK_PROGRESS_NOTIFICATION = alert.block_progress_notification;
     alert_category_t ALL_CATEGORIES = alert.all_categories;
 
     /**
--- src/main/java/org/libTAU4j/alerts/Alerts.java
+++ src/main/java/org/libTAU4j/alerts/Alerts.java
@@ -35,67 +35,67 @@ public final class Alerts {
         arr[0] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new UdpErrorAlert(cast_to_udp_error_alert(a));
             }
         };
         arr[1] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new ExternalIpAlert(cast_to_external_ip_alert(a));
             }
         };
         arr[2] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new UdpErrorAlert(cast_to_udp_error_alert(a));
+                return new ListenFailedAlert(cast_to_listen_failed_alert(a));
             }
         };
         arr[3] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new ExternalIpAlert(cast_to_external_ip_alert(a));
+                return new ListenSucceededAlert(cast_to_listen_succeeded_alert(a));
             }
         };
         arr[4] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new ListenFailedAlert(cast_to_listen_failed_alert(a));
+                return new PortmapErrorAlert(cast_to_portmap_error_alert(a));
             }
         };
         arr[5] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new ListenSucceededAlert(cast_to_listen_succeeded_alert(a));
+                return new PortmapAlert(cast_to_portmap_alert(a));
             }
         };
         arr[6] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new PortmapErrorAlert(cast_to_portmap_error_alert(a));
+                return new PortmapLogAlert(cast_to_portmap_log_alert(a));
             }
         };
         arr[7] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new PortmapAlert(cast_to_portmap_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[8] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new PortmapLogAlert(cast_to_portmap_log_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[9] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new DhtBootstrapAlert(cast_to_dht_bootstrap_alert(a));
             }
         };
         arr[10] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new SessionStartOverAlert(cast_to_session_start_over_alert(a));
             }
         };
         arr[11] = new CastLambda() {
@@ -107,19 +107,19 @@ public final class Alerts {
         arr[12] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new DhtBootstrapAlert(cast_to_dht_bootstrap_alert(a));
+                return new SessionStopOverAlert(cast_to_session_stop_over_alert(a));
             }
         };
         arr[13] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new SessionStartOverAlert(cast_to_session_start_over_alert(a));
+                return new SessionStatsAlert(cast_to_session_stats_alert(a));
             }
         };
         arr[14] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new DhtErrorAlert(cast_to_dht_error_alert(a));
             }
         };
         arr[15] = new CastLambda() {
@@ -131,49 +131,49 @@ public final class Alerts {
         arr[16] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new SessionStopOverAlert(cast_to_session_stop_over_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[17] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new SessionStatsAlert(cast_to_session_stats_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[18] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new DhtErrorAlert(cast_to_dht_error_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[19] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new LogAlert(cast_to_log_alert(a));
             }
         };
         arr[20] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new DhtStatsAlert(cast_to_dht_stats_alert(a));
             }
         };
         arr[21] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new DhtLogAlert(cast_to_dht_log_alert(a));
             }
         };
         arr[22] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new DhtPktAlert(cast_to_dht_pkt_alert(a));
             }
         };
         arr[23] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new LogAlert(cast_to_log_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[24] = new CastLambda() {
@@ -185,19 +185,19 @@ public final class Alerts {
         arr[25] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new DhtStatsAlert(cast_to_dht_stats_alert(a));
+                return new SessionErrorAlert(cast_to_session_error_alert(a));
             }
         };
         arr[26] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new DhtLogAlert(cast_to_dht_log_alert(a));
+                return handleUnknownAlert(a);
             }
         };
         arr[27] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new DhtPktAlert(cast_to_dht_pkt_alert(a));
+                return new SessionStatsHeaderAlert(cast_to_session_stats_header_alert(a));
             }
         };
         arr[28] = new CastLambda() {
@@ -209,7 +209,7 @@ public final class Alerts {
         arr[29] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new AlertsDroppedAlert(cast_to_alerts_dropped_alert(a));
             }
         };
         arr[30] = new CastLambda() {
@@ -221,76 +221,40 @@ public final class Alerts {
         arr[31] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new SessionErrorAlert(cast_to_session_error_alert(a));
+                return new CommNewDeviceIdAlert(cast_to_communication_new_device_id_alert(a));
             }
         };
         arr[32] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new CommNewMsgAlert(cast_to_communication_new_message_alert(a));
             }
         };
         arr[33] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new SessionStatsHeaderAlert(cast_to_session_stats_header_alert(a));
+                return new CommConfirmRootAlert(cast_to_communication_confirmation_root_alert(a));
             }
         };
         arr[34] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
+                return new CommSyncMsgAlert(cast_to_communication_syncing_message_alert(a));
             }
         };
         arr[35] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return new AlertsDroppedAlert(cast_to_alerts_dropped_alert(a));
+                return new CommFriendInfoAlert(cast_to_communication_friend_info_alert(a));
             }
         };
         arr[36] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
-                return handleUnknownAlert(a);
-            }
-        };
-        arr[37] = new CastLambda() {
-            @Override
-            public Alert cast(alert a) {
-                return new CommNewDeviceIdAlert(cast_to_communication_new_device_id_alert(a));
-            }
-        };
-        arr[38] = new CastLambda() {
-            @Override
-            public Alert cast(alert a) {
-                return new CommNewMsgAlert(cast_to_communication_new_message_alert(a));
-            }
-        };
-        arr[39] = new CastLambda() {
-            @Override
-            public Alert cast(alert a) {
-                return new CommConfirmRootAlert(cast_to_communication_confirmation_root_alert(a));
-            }
-        };
-        arr[40] = new CastLambda() {
-            @Override
-            public Alert cast(alert a) {
-                return new CommSyncMsgAlert(cast_to_communication_syncing_message_alert(a));
-            }
-        };
-        arr[41] = new CastLambda() {
-            @Override
-            public Alert cast(alert a) {
-                return new CommFriendInfoAlert(cast_to_communication_friend_info_alert(a));
-            }
-        };
-        arr[42] = new CastLambda() {
-            @Override
-            public Alert cast(alert a) {
                 return new CommLogAlert(cast_to_communication_log_alert(a));
             }
         };
-        arr[43] = new CastLambda() {
+        arr[37] = new CastLambda() {
             @Override
             public Alert cast(alert a) {
                 return new CommLastSeenAlert(cast_to_communication_last_seen_alert(a));
