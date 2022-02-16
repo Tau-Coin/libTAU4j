2bb0d3ae165014d223b7ecdb9f024a9372f25bc7
hankelc <taichenliu@imorpheus.io>
2022-02-16 13:31:08 +0800

    add get chain id in blockchain state alert

--- src/main/java/org/libTAU4j/alerts/BlockChainStateAlert.java
+++ src/main/java/org/libTAU4j/alerts/BlockChainStateAlert.java
@@ -1,21 +1,28 @@
 package org.libTAU4j.alerts;
 
 import org.libTAU4j.Account;
+import org.libTAU4j.Vectors;
 import org.libTAU4j.swig.blockchain_state_alert;
 
 public class BlockChainStateAlert extends AbstractAlert<blockchain_state_alert> {
 
-	Account  account;
+	byte[] chain_id;
+	Account account;
     String alertMsg;
 
     BlockChainStateAlert(blockchain_state_alert alert) {
         super(alert);
 		this.account = new Account(alert.getAct());
+        this.chain_id = Vectors.byte_vector2bytes(alert.getChain_id());
 		this.alertMsg = alert.message();
     }
 
     public Account get_account() {
         return this.account;
+    }
+
+    public byte[] get_chain_id() {
+        return this.chain_id;
     }
 
     public String get_message() {
