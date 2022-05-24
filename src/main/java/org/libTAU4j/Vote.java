219bc39f7c4e6005c81852b0fb43125baf8edbdb
hankelc <taichenliu@imorpheus.io>
2022-05-24 14:43:55 +0800

    modify vote with libTAU

--- src/main/java/org/libTAU4j/Vote.java
+++ src/main/java/org/libTAU4j/Vote.java
@@ -9,7 +9,7 @@ package org.libTAU4j;
 
 import org.libTAU4j.Entry;
 import org.libTAU4j.swig.vote;
-import org.libTAU4j.swig.sha256_hash;
+import org.libTAU4j.swig.block;
 
 import java.math.BigInteger;
 
@@ -19,58 +19,23 @@ import java.math.BigInteger;
  * @author taichen.liu
  */
 public final class Vote {
-
+    /*
 	private final sha256_hash block_hash;
 	private final BigInteger cumulative_difficulty;
 	private final long block_number;
+    */
+	private final block bk;
 	private final long count;
-
     private final vote vt;
-
-    public Vote(sha256_hash block_hash, BigInteger cumulative_difficulty, long block_number) {
-
-		this.block_hash = block_hash;
-		this.cumulative_difficulty = cumulative_difficulty;
-		this.block_number = block_number;
-		
-		this.vt = new vote(block_hash, cumulative_difficulty, block_number);
-		this.count = this.vt.count();
-	}
 
     public Vote(vote vt) {
 		this.vt = vt;
-		this.block_hash = vt.block_hash();
-		this.cumulative_difficulty = vt.cumulative_difficulty();
-		this.block_number = vt.block_number();
+		this.bk = vt.voting_block();
 		this.count = vt.count();
 	}
 
-    public Vote(Entry e) {
-		this.vt = new vote(e.swig());
-		this.block_hash = vt.block_hash();
-		this.cumulative_difficulty = vt.cumulative_difficulty();
-		this.block_number = vt.block_number();
-		this.count = vt.count();
-	}
-
-    public Vote(String s) {
-		this.vt = new vote(s);
-		this.block_hash = vt.block_hash();
-		this.cumulative_difficulty = vt.cumulative_difficulty();
-		this.block_number = vt.block_number();
-		this.count = vt.count();
-	}
-
-  	public sha256_hash getBlockHash() {
-    	return this.block_hash;
-  	}
-
-  	public BigInteger getCumulativeDifficulty() {
-    	return this.cumulative_difficulty;
-  	}
-
-  	public long getBlockNumber() {
-    	return this.block_number;
+  	public Block getBlock() {
+    	return new Block(this.bk);
   	}
 
   	public long getVoteCount() {
@@ -80,5 +45,4 @@ public final class Vote {
 	public vote swig() {
     	return this.vt;
 	}
-
 }
