75865c689b28e3744e95ac1bdaeeb8f3f1971da0
hankelc <taichenliu@imorpheus.io>
2022-07-15 17:05:28 +0800

    add get key in user info alert

--- src/main/java/org/libTAU4j/alerts/CommUserInfoAlert.java
+++ src/main/java/org/libTAU4j/alerts/CommUserInfoAlert.java
@@ -6,6 +6,7 @@ import org.libTAU4j.swig.communication_user_info_alert;
 public final class CommUserInfoAlert extends AbstractAlert<communication_user_info_alert> {
 
 	byte[] peer;
+	byte[] key;
 	byte[] userInfo;
 	String alertMsg;
 
@@ -13,12 +14,17 @@ public final class CommUserInfoAlert extends AbstractAlert<communication_user_in
         super(alert);
 
 		this.peer = Vectors.byte_vector2bytes(alert.getPeer().to_bytes());
+        this.key = Vectors.byte_vector2bytes(alert.getKey());
         this.userInfo = Vectors.byte_vector2bytes(alert.getUser_info());
 		this.alertMsg = alert.message();
     }
 
     public byte[] get_peer() {
 		return this.peer;
+    }
+
+    public byte[] get_key() {
+		return this.key;
     }
 
     public byte[] get_user_info() {
