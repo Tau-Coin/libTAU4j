8aab5ee785d81bc10099f010fd0874547ad2b9dc
hankelc <taichen.liu@imorpheus.io>
2022-10-26 10:56:43 +0800

    add arm64 ndk24

---
+++ swig/build-android-arm64-ndk24.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+source build-utils-arm64-ndk24.shinc
+check_min_req_vars
+
+export os_arch=arm64
+export os_build=android
+export android_api=24
+export SHARED_LIB=${LIBRARY_NAME}.so
+export SHARED_LIB_FINAL=${SHARED_LIB} # dummy for macosx
+export CXX=g++
+export NDK_VERSION=r24
+prepare_android_toolchain
+abort_if_var_unset "ANDROID_TOOLCHAIN" ${ANDROID_TOOLCHAIN}
+export CC=$ANDROID_TOOLCHAIN/bin/aarch64-linux-android-clang
+export run_openssl_configure="./Configure linux-aarch64 ${OPENSSL_NO_OPTS} -march=armv8-a+crypto -mfpu=neon -fPIC --prefix=${OPENSSL_ROOT}";
+export run_readelf="${ANDROID_TOOLCHAIN}/bin/llvm-readelf -d bin/release/${os_build}/${os_arch}-v8a/${SHARED_LIB}"
+export run_bjam="${BOOST_ROOT}/b2 -j4 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=clang-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}-v8a"
+export run_strip="${ANDROID_TOOLCHAIN}/bin/llvm-strip --strip-unneeded -x -g bin/release/${os_build}/${os_arch}-v8a/${SHARED_LIB}"
+export run_objcopy="${ANDROID_TOOLCHAIN}/bin/llvm-objcopy --only-keep-debug bin/release/${os_build}/${os_arch}-v8a/${SHARED_LIB} bin/release/${os_build}/${os_arch}-v8a/${SHARED_LIB}.debug"
+export PATH=$ANDROID_TOOLCHAIN/aarch64-linux-android/bin:$PATH;
+sed -i 's/RANLIB = ranlib/RANLIB = "${ANDROID_TOOLCHAIN}\/bin\/llvm-ranlib"/g' ${BOOST_ROOT}/tools/build/src/tools/gcc.jam;
+
+create_folder_if_it_doesnt_exist ${SRC}
+prompt_msg "About to prepare BOOST ${BOOST_VERSION}"
+press_any_to_continue
+#prepare_boost
+#prepare_openssl
+#build_openssl
+#prepare_libTAU
+build_libraries
+#cleanup_objects
